8a6e07eae4af97e495047d655590e90b144022a7
Aflak Arshi <aflak.arshi@encureit.com>
2018-06-11 11:56:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'News App';
+  news = {};
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit() {
+    this.getNews();
+  }
+
+  getNews() {
+    this.http.get(`https://hn.algolia.com/api/v1/search_by_date?query=nodejs&tags=story`)
+      .subscribe(data => {
+        this.news = data
+      });
+  }
 }
